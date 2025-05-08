79dd0a9637e8099212bd622145c757cbd3349857
Miguel Urena <urena_miguel82@outlook.com>
2025-05-08 13:02:58 -0700

    Confetti animation

--- script.js
+++ script.js
@@ -9,4 +9,13 @@ function celebrate() {
     spread: 70,
     origin: { y: 0.6 },
   });
+
+  // Optionally, repeat a few bursts
+  setTimeout(() => {
+    confetti({ particleCount: 70, spread: 90, origin: { y: 0.6 } });
+  }, 400);
+
+  setTimeout(() => {
+    confetti({ particleCount: 50, spread: 100, origin: { y: 0.6 } });
+  }, 800);
 }
